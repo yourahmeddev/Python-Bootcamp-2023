064151aaa8737a684503549e0bd13f9323877b2b
Ahmed Hassan Sheikh <114868725+yourahmeddev@users.noreply.github.com>
2022-12-25 07:46:11 +0500

    Add files via upload

---
+++ Lecture#1 Python Bootcamp 2023.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello world")
+
+
+# # Excercise#1
+
+# In[ ]:
+
+
+#You have to print your name, father name , your school, university college
+
