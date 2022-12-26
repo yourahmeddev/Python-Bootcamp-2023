66aeda4366265a3f3a8c4c9e8e9d678344719c07
Ahmed Hassan Sheikh <114868725+yourahmeddev@users.noreply.github.com>
2022-12-26 11:37:09 +0500

    Add files via upload

---
+++ Lecture#2 Python Bootcamp 2023.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#creating variable
+username="Ahmed"
+userage=22
+print(username)
+print(userage)
+
+
+# In[14]:
+
+
+age=22
+Age=30
+AGE=40
+print(age)
+print(Age)
+print(AGE)
+
+
+# In[17]:
+
+
+name="Ahmed"
+print(name)
+name=90
+print(name)
+
+
+# In[23]:
+
+
+age=int(22)
+print(age)
+name=str("Ahmed Hasssan Sheikh")
+print(name)
+pi=float(3.14)
+print(pi)
+
+
+# In[25]:
+
+
+x,y,z="Ahmed","Hassan","Sheikh"
+print(x)
+print(y)
+print(z)
+
