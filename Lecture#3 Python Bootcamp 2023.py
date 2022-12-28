1511cdddef6f6e589954016a1f6726418863a0ee
Ahmed Hassan Sheikh <114868725+yourahmeddev@users.noreply.github.com>
2022-12-28 23:09:39 +0500

    Add files via upload

---
+++ Lecture#3 Python Bootcamp 2023.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#we are learning python programming
+
+
+# In[5]:
+
+
+#my name is ahmed hassan 
+
+
+# In[7]:
+
+
+#data types
+a1=300
+print(a1)
+print(type(a1))
+
+
+# In[9]:
+
+
+#data types
+a2=3.14
+print(a2)
+print(type(a2))
+
+
+# In[11]:
+
+
+#data types
+a3=True
+print(a3)
+print(type(a3))
+
+
+# In[13]:
+
+
+#data types
+a4="Python with ahmed"
+print(a4)
+print(type(a4))
+
+
+# In[15]:
+
+
+#data types
+a5=5+4j
+print(a5)
+print(type(a5))
+
