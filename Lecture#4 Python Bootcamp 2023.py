7a47047f7fda0516ad9531e3818a99ec4b75f9ab
Ahmed Hassan Sheikh <114868725+yourahmeddev@users.noreply.github.com>
2022-12-30 11:22:44 +0500

    Add files via upload

---
+++ Lecture#4 Python Bootcamp 2023.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#keywords in python
+#True=22
+
+
+# In[10]:
+
+
+#arthOperators
+x=10
+y=20
+print(x+y)
+print("-------------------------")
+print(x-y)
+print("-------------------------")
+print(x*y)
+print("-------------------------")
+print(x/y)
+
+
+# In[18]:
+
+
+#Relationaloperators
+x=20
+y=10
+print(x<y)
+print("-------------------")
+print(x>y)
+print("--------------------")
+print(x==y)
+print("-----------------")
+print(x!=y)
+
+
+# In[25]:
+
+
+#logicaloperators
+#and operators
+x=True
+y=False
+print(x&x)
+print("-------------------")
+print(x&y)
+print("-----------------------")
+print(y&x)
+print("---------------------")
+print(y&y)
+
+
+# In[33]:
+
+
+#logicaloperators
+#or operators
+x=True
+y=False
+print(x|x)
+print("--------------")
+print(x|y)
+print("----------------")
+print(y|x)
+print("----------------")
+print(y|y)
+
